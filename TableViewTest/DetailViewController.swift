808fa3bafbe7e75731558f3675dea2c92bee4d71
Flutter_SakuSaku13:44 <gitonly543@gmail.com>
2022-02-20 16:13:02 +0900

    制約をつけた

---
+++ TableViewTest/DetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailViewController.swift
+//  TableViewTest
+//
+//  Created by 橋本純一 on 2022/02/20.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
